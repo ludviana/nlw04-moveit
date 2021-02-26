3ecde7ca364dce02255d045997e65b7dd82da696
Lucas Damasceno <lucasd.viana1993@gmail.com>
2021-02-26 14:12:29 -0300

    25 min

--- src/context/CountDownContext.tsx
+++ src/context/CountDownContext.tsx
@@ -21,7 +21,7 @@ export const CountDownContext = createContext({} as CountDownContextData)
 export function CountDownProvider({ children }: CountDownProviderProps){
   const { startNewChallenge } = useContext(challengesContext)
 
-  const [time, setTime] = useState(0.05 * 60)
+  const [time, setTime] = useState(25 * 60)
   const [isActive, setIsActive] = useState(false)
   const [hasFinished, setHasFinished] = useState(false)
 
